0fb66f6493eca5ec1dda336534ebfa542da7a1bd
alexander <avoronin2004@gmail.com>
2022-12-12 14:16:48 +0300

    :)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to root_path, notice: 'Logged in successfully'
     else
       flash.now[:alert] = 'Invalid email or password'
-      render 'users/new'
+      render 'new'
     end
   end
   def destroy
