1eed38e17fa9ba8ea661617fae3704422c0d7ec6
alexander <avoronin2004@gmail.com>
2022-12-23 20:11:42 +0300

    validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,7 +4,7 @@ class Record < ApplicationRecord
   has_one_attached :photo
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { maximum: 30 }
-  validates :photo, presence: true, length: { maximum: 255 }
-  validates :description, presence: true
+  validates :photo, presence: true
+  validates :description, presence: true, length: { maximum: 255 }
   validates :status, presence: true
 end
