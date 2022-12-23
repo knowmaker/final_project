243acb50c8703e06b0054b96bd838e8c426ee633
alexander <avoronin2004@gmail.com>
2022-12-24 00:21:37 +0300

    welcome mail locales

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,10 +2,9 @@
 
 # class WelcomeMailer
 class WelcomeMailer < ApplicationMailer
-  # sends a welcome email
   def welcome_email
     @user = params[:user]
     @url = 'http://localhost:3000/sign_in'
-    mail(to: @user.email, subject: 'Welcome to my project')
+    mail(to: @user.email, subject: (t 'global.sub_welc'))
   end
 end
